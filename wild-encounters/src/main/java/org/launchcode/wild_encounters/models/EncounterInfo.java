c2eb7ab7d7e75692656340cee69d98801f672b1b
codegabb <gab.blenden@gmail.com>
2024-07-25 18:09:25 -0500

    Setup Enounter controller, model, and repository

--- wild-encounters/src/main/java/org/launchcode/wild_encounters/models/EncounterInfo.java
+++ wild-encounters/src/main/java/org/launchcode/wild_encounters/models/EncounterInfo.java
@@ -1,5 +1,13 @@
 package org.launchcode.wild_encounters.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class EncounterInfo {
+    @Id
+    @GeneratedValue
+    private Long id;
 
 }
