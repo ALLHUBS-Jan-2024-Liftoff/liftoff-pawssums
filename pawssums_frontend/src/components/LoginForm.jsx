c4e0c81ef40598dc33b1155c0b199e8612059fcc
celiapaivab <celiadepaivabruno@gmail.com>
2024-07-28 18:55:35 -0500

    Adding css to login form

--- pawssums_frontend/src/components/LoginForm.jsx
+++ pawssums_frontend/src/components/LoginForm.jsx
@@ -17,7 +17,8 @@ export const LoginForm = () => {
 
     return(
         <div className="d-flex justify-content-center align-items-center vh-100">
-            <form onSubmit={handleSubmit}>
+            <div className="bg-light p-3 border rounded w-25">
+                <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label class="form-label" htmlFor="email">E-mail</label>
                     <input class="form-control" type="email" id="email" name="email" placeholder="Enter e-mail" onChange={handleInput} ></input>
@@ -26,10 +27,11 @@ export const LoginForm = () => {
                     <label class="form-label" htmlFor="password">Password</label>
                     <input class="form-control" type="password" id="password" name="password" placeholder="Enter password" onChange={handleInput}  ></input>
                 </div>
-                <button className="btn btn-default border" type="submit">Sign in</button>
-                <p>Don't have an account?</p>
-                <button className="btn btn-default border">Register</button>
+                <button className="btn btn-success border w-100" type="submit">Sign in</button>
+                <button className="btn btn-link w-100">Don't have an account? Register</button>
             </form>
+            </div>
+            
         </div>
     )
 }
