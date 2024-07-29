eae2a89887f210c84bc874efd7dcf5fdec359ea4
celiapaivab <celiadepaivabruno@gmail.com>
2024-07-29 16:18:19 -0500

    Added basic home page #25

--- pawssums_frontend/src/App.jsx
+++ pawssums_frontend/src/App.jsx
@@ -26,7 +26,6 @@ function App() {
       </div>
     </APIProvider>
 
-      <h1>WILD ENCOUNTERS</h1>
       <Home/>
       <Login/>
       <Register/>
--- pawssums_frontend/src/components/pages/Home.jsx
+++ pawssums_frontend/src/components/pages/Home.jsx
@@ -3,8 +3,13 @@
 export const Home = () => {
 
     return(
-        <div>
-            <h1>Home</h1>
+        <div class="d-grid justify-content-center p-5">
+            <div className="bg-light p-3 border rounded m-5">
+                <h1 className="text-center p-2">WILD ENCOUNTERS</h1>
+            </div>
+            <div className="bg-light p-3 border rounded m-5 p-5">
+                <h1 className="text-center border ">MAP</h1>
+            </div>
         </div>
     )
 }
