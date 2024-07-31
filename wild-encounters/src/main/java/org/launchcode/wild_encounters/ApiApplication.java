da30f61cc80759127c98b8578aeeae155f02037a
codegabb <gab.blenden@gmail.com>
2024-07-31 06:28:56 -0500

    Added encounterService folder to front end for axios import

--- wild-encounters/src/main/java/org/launchcode/wild_encounters/ApiApplication.java
+++ wild-encounters/src/main/java/org/launchcode/wild_encounters/ApiApplication.java
@@ -7,24 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
-public class ApiApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
-    }
+@Configuration
+public class ApiApplication implements WebMvcConfigurer {
 
-    // To prevent CORS issue with local development
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
