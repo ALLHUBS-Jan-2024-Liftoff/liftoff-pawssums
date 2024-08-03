42a355ce00a05e1cfada52143b884938d79500df
celiapaivab <celiadepaivabruno@gmail.com>
2024-08-02 21:23:13 -0500

    Added links for profile and encounter to navbar #9

--- pawssums_frontend/src/components/Navbar.jsx
+++ pawssums_frontend/src/components/Navbar.jsx
@@ -3,13 +3,8 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
-            <a className="navbar-brand mb-0 mt-0 m-3 h1 border" href="#">
-                Wild Encounters
-            </a>
+            <Link to="/home" className="navbar-brand mb-0 mt-0 m-3 h1 border" href="#">Wild Encounters</Link>
             <ul className="navbar-nav mb-0 mt-0 m-3">
-                <li className="nav-item">
-                    <Link to="/home" className="nav-link text-decoration-none">Home</Link>
-                </li>
                 <li className="nav-item">
                     <Link to="/login" className="nav-link text-decoration-none">Login</Link>
                 </li>
@@ -18,6 +13,9 @@ export const Navbar = () => {
                 </li>
                 <li className="nav-item">
                     <Link to="/profile" className="nav-link text-decoration-none">Profile</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/encounter-list" className="nav-link text-decoration-none">Encounters</Link>
                 </li>
                 <li className="nav-item">
                     <Link to="/search" className="nav-link text-decoration-none">Search</Link>
