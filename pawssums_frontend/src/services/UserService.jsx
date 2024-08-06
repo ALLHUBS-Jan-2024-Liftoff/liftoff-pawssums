fabc4efbd8fe121b022fd240200828330b88d9cb
celiapaivab <celiadepaivabruno@gmail.com>
2024-08-05 19:56:25 -0500

    Finished authentication #23

--- pawssums_frontend/src/services/UserService.jsx
+++ pawssums_frontend/src/services/UserService.jsx
@@ -1,36 +1,3 @@
-// import axios from 'axios';
-
-// const BASE_URL = 'http://localhost:8080/api/user';
-
-// export const registerUser = async (name, email, password) => {
-//     try {
-//         const response = await axios.post(`${BASE_URL}/newUser`, null, {
-//             params: {
-//                 name,
-//                 email,
-//                 password
-//             }
-//         });
-//         return response.data;
-//     } catch (error) {
-//         console.error('Registration error:', error);
-//         throw error;
-//     }
-// };
-
-// export const loginUser = async (email, password) => {
-//     try {
-//         const response = await axios.post(`${BASE_URL}/login`, null, {
-//             params: { email, password },
-//         });
-//         return response.data;
-//     } catch (error) {
-//         console.error('Login error:', error);
-//         throw error;
-//     }
-// };
-
-
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:8080/api/user';
